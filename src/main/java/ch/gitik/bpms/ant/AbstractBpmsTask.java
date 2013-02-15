be30f3534c8caa401f6e2976b8a737be00b40f34
Roland Gisler <rgisler@swissonline.ch>
2013-02-15 22:02:49 +0100

    [#5] CleanCode PMD Issues https://github.com/rgisler/bpms/issues/issues/issue/5

--- src/main/java/ch/gitik/bpms/ant/AbstractBpmsTask.java
+++ src/main/java/ch/gitik/bpms/ant/AbstractBpmsTask.java
@@ -16,7 +16,6 @@
 package ch.gitik.bpms.ant;
 
 import org.apache.tools.ant.BuildEvent;
-import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.BuildListener;
 import org.apache.tools.ant.Task;
 
@@ -25,9 +24,8 @@ import ch.gitik.bpms.common.MessageFactory;
 import ch.gitik.bpms.common.MessageType;
 
 /**
- * ANT-Task fuer BPMS (Build Process Messaging System). Dieser ANT-Task kann in
- * ein build.xml eingebunden werden und versendet dann Messages an interessierte
- * Benutzer.
+ * ANT-Task fuer BPMS (Build Process Messaging System). Dieser ANT-Task kann in ein build.xml eingebunden werden und
+ * versendet dann Messages an interessierte Benutzer.
  * @author Roland Gisler
  */
 public abstract class AbstractBpmsTask extends Task implements BuildListener {
@@ -42,26 +40,20 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
    private String group = "bpms";
 
    /** AutoNotify default nicht aktiviert. */
-   private boolean autonotify = false;
+   private boolean autonotify;
 
    private String lastTarget = "";
 
    /**
     * Check the input and throw a BuildException when it is null.
-    * @throws BuildException
-    *            Buildfehler.
-    * @throws IllegalArgumentException
-    *            Fehlendes Attribut.
     */
-   public abstract void checkAttributes() throws BuildException;
+   public abstract void checkAttributes();
 
    /**
-    * Execute-Methode des Ant-Tasks. Diese Methode prueft ob alle Argemente
-    * vorhanden sind und sendet dann eine BPMS-Message.
-    * @throws BuildException
-    *            on wrong argument or IO error.
+    * Execute-Methode des Ant-Tasks. Diese Methode prueft ob alle Argemente vorhanden sind und sendet dann eine
+    * BPMS-Message.
     */
-   public void execute() throws BuildException {
+   public final void execute() {
 
       // Parameter pruefen
       this.checkAttributes();
@@ -78,10 +70,10 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
       }
    }
 
-   /**
+   /*
     * @see org.apache.tools.ant.BuildListener#buildStarted(org.apache.tools.ant.BuildEvent)
     */
-   public void buildStarted(BuildEvent arg0) {
+   public void buildStarted(final BuildEvent arg0) {
       Message msg = this.messageFactory.createMessage();
       msg.setProject(arg0.getProject().getName());
       msg.setTask("start");
@@ -89,10 +81,10 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
       this.sendMessage(msg);
    }
 
-   /**
+   /*
     * @see org.apache.tools.ant.BuildListener#buildFinished(org.apache.tools.ant.BuildEvent)
     */
-   public void buildFinished(BuildEvent arg0) {
+   public void buildFinished(final BuildEvent arg0) {
       String result = "BUILD ";
       if (arg0.getException() == null) {
          result += "SUCCESSFUL";
@@ -106,18 +98,18 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
       this.sendMessage(msg);
    }
 
-   /**
-    * @see org.apache.tools.ant.BuildListener#targetStarted(org.apache.tools.ant.BuildEvent)
+   /*
+    * @see org.apache.tools.ant.BuildListener#targetStarted(org.apache.tools.ant. BuildEvent)
     */
-   public void targetStarted(BuildEvent arg0) {
+   public void targetStarted(final BuildEvent arg0) {
       this.lastTarget = arg0.getTarget().getName();
       this.sendMessage(prepareMessage(arg0.getProject().getName(), "Target wird bearbeitet..."));
    }
 
-   /**
-    * @see org.apache.tools.ant.BuildListener#targetFinished(org.apache.tools.ant.BuildEvent)
+   /*
+    * @see org.apache.tools.ant.BuildListener#targetFinished(org.apache.tools.ant .BuildEvent)
     */
-   public void targetFinished(BuildEvent arg0) {
+   public void targetFinished(final BuildEvent arg0) {
       if (!this.lastTarget.equals(arg0.getTarget().getName())) {
          this.lastTarget = arg0.getTarget().getName();
          this.sendMessage(prepareMessage(arg0.getProject().getName(), "Target wurde bearbeitet."));
@@ -132,7 +124,7 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
     *           Messagetext.
     * @return Message Message.
     */
-   private Message prepareMessage(String project, String text) {
+   private Message prepareMessage(final String project, final String text) {
       Message msg = this.messageFactory.createMessage();
       msg.setProject(project);
       msg.setTask(this.lastTarget);
@@ -140,23 +132,25 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
       return msg;
    }
 
-   /**
-    * @see org.apache.tools.ant.BuildListener#taskStarted(org.apache.tools.ant.BuildEvent)
-    *      .
+   /*
+    * @see org.apache.tools.ant.BuildListener#taskStarted(org.apache.tools.ant.BuildEvent )
     */
-   public void taskStarted(BuildEvent arg0) {
+   public void taskStarted(final BuildEvent arg0) {
+      // NOPMD do nothing.
    }
 
-   /**
-    * @see org.apache.tools.ant.BuildListener#taskFinished(org.apache.tools.ant.BuildEvent)
+   /*
+    * @see org.apache.tools.ant.BuildListener#taskFinished(org.apache.tools.ant. BuildEvent)
     */
    public void taskFinished(final BuildEvent arg0) {
+      // NOPMD do nothing.
    }
 
-   /**
-    * @see org.apache.tools.ant.BuildListener#messageLogged(org.apache.tools.ant.BuildEvent)
+   /*
+    * @see org.apache.tools.ant.BuildListener#messageLogged(org.apache.tools.ant. BuildEvent)
     */
    public void messageLogged(final BuildEvent arg0) {
+      // NOPMD do nothing.
    }
 
    /**
@@ -175,7 +169,7 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
     * Liefert AutoNotify zurueck.
     * @return boolean
     */
-   public boolean isAutonotify() {
+   public final boolean isAutonotify() {
       return autonotify;
    }
 
@@ -184,7 +178,7 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
     * @param autonot
     *           boolean
     */
-   public void setAutonotify(boolean autonot) {
+   public final void setAutonotify(final boolean autonot) {
       this.autonotify = autonot;
    }
 
@@ -192,7 +186,7 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
     * Liefert die Gruppe.
     * @return String Gruppe.
     */
-   public String getGroup() {
+   public final String getGroup() {
       return group;
    }
 
@@ -201,7 +195,7 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
     * @param group
     *           String.
     */
-   public void setGroup(String group) {
+   public final void setGroup(final String group) {
       this.group = group;
    }
 
@@ -209,7 +203,7 @@ public abstract class AbstractBpmsTask extends Task implements BuildListener {
     * Liefert die MessageFactory.
     * @return MessageFactory.
     */
-   public MessageFactory getMessageFactory() {
+   public final MessageFactory getMessageFactory() {
       return messageFactory;
    }
 }
